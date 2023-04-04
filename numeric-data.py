ea379283810004edd754a60362c11984fed6c0e2
Dragana Spasojevic <draganaspasojevic@Draganas-Air.fritz.box>
2023-04-04 15:18:59 +0200

    exercise in python

---
+++ numeric-data.py
@@ -0,0 +1,16 @@
+print(4 - 2)
+print (2 * 2)
+print("Python has three numeric types: int, float, and complex")
+myValue=1
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+myValue1 = 3.14
+print(myValue1)
+print(type(myValue1))
+print(str(myValue1) + " is of the data type " + str(type(myValue1)))
+myValue=5j
+print(myValue)
+print(type(myValue))
+myValue=True
+print(myValue)
